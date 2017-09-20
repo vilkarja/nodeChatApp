da4d96d26ae85f830113c3cfe5e7c79eb0a422f5
Ville Karjalainen <ville.karjalainen@student.tut.fi>
2017-09-20 19:58:27 +0300

    Scroll to bottom if user is near bottom

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user conencted');
+    console.log('New user connected');
     
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the lobby'));
