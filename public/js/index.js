be745420fa58c0aaa2a21edbc6b2f58374189db9
Ville Karjalainen <ville.karjalainen@student.tut.fi>
2017-09-20 16:39:36 +0300

    Form and button to send the messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,27 @@
 
 var socket = io();
 socket.on('connect', function() {
-    console.log('Connected to server');
-
-    
+    console.log('Connected to server'); 
 });
+
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function(message) {
     console.log('New message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    console.log(li);
+    jQuery('#messages').append(li);
+});
 
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'user',
+        text: jQuery('[name=message').val()
+    }, function () {
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -19,17 +19,10 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the lobby'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-
-
         io.emit('newMessage', generateMessage(message.from, message.text));
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
-
+        callback('This is callback from the server');
     });
 
     socket.on('disconnect', () => {
