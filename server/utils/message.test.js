9b75f60fed98626928e9fdfd658bd275d0388158
Ville Karjalainen <ville.karjalainen@student.tut.fi>
2017-09-20 18:07:14 +0300

    test for location

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -9,6 +9,20 @@ describe('generateMessage', () => {
         
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text});
+        
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate current lcoation object', () => {
+        var from = 'Someone';
+        var latitude = 1;
+        var longitude = 1;
+        var url = 'https://www.google.com/maps?q1,1';
+        var message = generateLocationMessage(from, latitude, longitude);
+        
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
         
     });
 });
