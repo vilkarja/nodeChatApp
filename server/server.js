e8c82a731ccbad31e94b3d33cbb313592f3d473b
Ville Karjalainen <ville.karjalainen@student.tut.fi>
2017-09-20 21:52:58 +0300

    send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -59,7 +59,6 @@ io.on('connection', (socket) => {
 
         if (user) {
             io.to(user.room).emit('updateUserList', users.getUserList(user.room));
-            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left the room`));
         }
     });
 });
