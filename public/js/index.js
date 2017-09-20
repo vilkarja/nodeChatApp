1e63fe33f20ea1fdb918b4fe1f27d3a42fa95c6d
Ville Karjalainen <ville.karjalainen@student.tut.fi>
2017-09-20 18:59:36 +0300

    timestamps for messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,19 +9,20 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('New message', message);
+    var formattedTime = moment(message.createdAt).format('H:mm');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     console.log(li);
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('H:mm');
     var li = jQuery('<li></li>');
     var a = jQuery('<a rel="noopener noreferrer" target="_blank">My current location</a>');
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
-    console.log(li);
+    
     li.append(a);
     jQuery('#messages').append(li);
 });
